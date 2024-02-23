011612b68a349b230b4428065785036f3de06036
Vedant-Nagarkar <vedantnagarkar01@gmail.com>
2024-02-23 11:11:08 +0530

    folder structure created

--- template.py
+++ template.py
@@ -16,7 +16,7 @@ list_of_files = [
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
     f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}config/configuration.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py"
@@ -43,7 +43,7 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
-        logging.info(f"Creating empty file:{filepath}")
+            logging.info(f"Creating empty file:{filepath}")
 
 
     else:
